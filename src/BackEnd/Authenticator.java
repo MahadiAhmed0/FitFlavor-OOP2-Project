1bb7de6ba2c714dcd03821224c3e3e7662936388
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2025-04-17 00:32:45 +0600

    Authenticator - added file write,read, account creation methods

--- src/BackEnd/Authenticator.java
+++ src/BackEnd/Authenticator.java
@@ -1,5 +1,6 @@
 package BackEnd;
 
+import java.io.*;
 import java.util.HashMap;
 
 public class Authenticator {
@@ -18,5 +19,39 @@ public class Authenticator {
     public boolean isPasswordValid(String password){
         return password.length() >= 6;
     }
-    
+
+    public void addUser(User user) {
+        userDatabase.put(user.getUsername(), user);
+        saveUserToFile(user);
+    }
+
+    public User getUser(String username) {
+        return userDatabase.get(username);
+    }
+
+    public boolean authenticate(String username, String password){
+        User user = getUser(username);
+        return user != null && user.checkPassword(password);
+    }
+
+    public void saveUserToFile(User user) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(File_Name, true))){
+            writer.write(user.getUsername() + "," + user.getPassword() + "," + user.getAge() + "," + user.getWeight());
+            writer.newLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void loadUsersFromFile() {
+        try(BufferedReader reader = new BufferedReader(new FileReader(File_Name))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] data = line.split(",");
+                User user = new User(data[0], data[1], Integer.parseInt(data[2]), Double.parseDouble(data[3]));
+                userDatabase.put(user.getUsername(), user);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
