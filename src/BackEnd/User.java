64a2108656aa18140c9453cd06755c26d45f944d
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2025-04-16 23:52:05 +0600

    added user class - datas and constructor

--- src/BackEnd/User.java
+++ src/BackEnd/User.java
@@ -1,4 +1,15 @@
 package BackEnd;
 
 public class User {
+    private String username, password;
+    private int age;
+    private double weight;
+
+    public User(String username, String password, int age, double weight) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.weight = weight;
+    }
+
 }
