112dc3227b89683a9f2a32371f22d31ed4a0d6bb
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2025-04-16 23:59:50 +0600

    Authenticator - added constructor and datas

--- src/BackEnd/Authenticator.java
+++ src/BackEnd/Authenticator.java
@@ -1,4 +1,14 @@
 package BackEnd;
 
+import java.util.HashMap;
+
 public class Authenticator {
+    private HashMap<String, User> userDatabase;
+    private static final String File_Name = "user_database.txt";
+
+    public Authenticator() {
+        userDatabase = new HashMap<>();
+        loadUsersFromFile();
+    }
+    
 }
